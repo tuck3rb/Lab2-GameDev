dab3e0c5aa7a2edca46c7f0e542c1be5201bbb54
Tucker Brock <brocktk@hendrix.edu>
2023-02-14 14:39:43 -0600

    FOOM

--- Lab2Unity/Assets/Scripts/Fireplace.cs
+++ Lab2Unity/Assets/Scripts/Fireplace.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fireplace : MonoBehaviour
-{
+public class Fireplace : MonoBehaviour {
+
+    [SerializeField] private AudioSource fire_noise;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fire_noise = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,6 +18,14 @@ public class Fireplace : MonoBehaviour
         if (Input.GetMouseButtonDown(0)) {
             // GameManager.Instance.ClickFireplace(scene);
             print("clicked!");
+            StartCoroutine("FireSound");
         }
+    }
+
+    IEnumerator FireSound()
+    {
+        fire_noise.Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        
     }
 }
